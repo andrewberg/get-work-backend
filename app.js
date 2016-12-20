01e14f3d08077bee6b291cae887fa29aa2af6cfc
Andrew Berg <andrewberg@pobox.com>
2016-12-19 21:25:06 -0500

    Adding support for GeoSpatial on route getoffers and updated post and get for router joboffers

--- app.js
+++ app.js
@@ -26,12 +26,22 @@ app.listen(3000);
 console.log("Server started.");
 
 mongoose.connect('mongodb://localhost/getwork');
+//mongoose.connection.db.ensureIndex({ 'loc': '2dsphere'});
 
 router.route('/joboffers').post(function(req, res) {
 	var job = new JobPosting();
+	var coords = req.body.coords.split(',').map(Number);
+	var expiretime = req.body.expiretime;
+	
 	job.JobTitle = req.body.title;
 	job.JobDescription = req.body.desc;
-	console.log(req.body);
+	job.geo = [coords[0], coords[1]];
+	job.JobPostDate = Date();
+	job.JobFilled = false;
+	job.JobExpireDate = new Date(expiretime);
+	job.JobPay = req.body.pay;
+	job.JobBossID = req.body.jobbossid;
+
 	job.save(function(err) {
 		if (err)
 			res.send(err);
@@ -40,9 +50,7 @@ router.route('/joboffers').post(function(req, res) {
 	});
 
 })
-
 .get(function(req, res) {
-	JobPosting.remove({});
 	JobPosting.find(function(err, jobs) {
 		if (err)
 			res.send(err);
@@ -50,8 +58,31 @@ router.route('/joboffers').post(function(req, res) {
 	});
 });
 
+router.route('/getoffers').post(function(req, res) {
+	var job = new JobPosting();
+	var distance = req.body.distance;
+	var coords = req.body.coords.split(',').map(Number);
 
 
+	JobPosting.find({
+		loc: {
+			$near: {
+				$geometry: {
+					type: "Point",
+					coordinates: coords
+				},
+				$maxDistance: distance
+			}
+		}
+
+	}).limit(10).exec(function(err, jobs) {
+		if (err) {
+			return res.json(500, err);
+		}
+		res.json(jobs);
+	});
+
+});
 
 app.use('/api', router);
 
--- models/jobposting.js
+++ models/jobposting.js
@@ -3,7 +3,14 @@ var Schema = mongoose.Schema;
 
 var JobPostingSchema = new Schema({
 	JobTitle: String,
-	JobDescription: String
+	JobDescription: String,
+	JobExpireDate: Date,
+	JobPostDate: Date,
+	JobPay: String,
+	JobBossID: Number,
+	JobFilled: Boolean,
+	geo: { type: [Number]}
 });
 
+JobPostingSchema.index({ loc : '2dsphere' });
 module.exports = mongoose.model('JobPosting', JobPostingSchema);
